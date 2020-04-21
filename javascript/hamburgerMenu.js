3ee0cd084480c9e842c4c908c91a47721f18d956
aaronburmeister <f34r91@gmail.com>
2020-04-21 14:14:45 -0600

    Finished a menu transition for mobile

--- javascript/hamburgerMenu.js
+++ javascript/hamburgerMenu.js
@@ -1,6 +1,21 @@
-const $hamburger = document.querySelector('.menu')
+const $hamburger = document.querySelector('nav div')
 $hamburger.addEventListener('click', showBurger)
 
 function showBurger () {
-    rgb
+    const lis = document.querySelectorAll('li a')
+    if (lis[0].classList.contains('open')) {
+        let anim = 900
+        lis.forEach( link => {
+            link.style.animationDuration = anim + "ms"
+            link.classList = "close"
+            anim -= 150
+        })
+    } else {
+        let anim = 300
+        lis.forEach( link => {
+            link.style.animationDuration = anim + "ms"
+            link.classList = 'open'
+            anim += 150
+        })
+    }
 }
